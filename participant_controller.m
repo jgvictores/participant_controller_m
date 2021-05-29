d0dff2fa66015c3aae755b6ec2ff428372573ede
jgvictores <jcgvicto@gmail.com>
2021-05-29 17:58:47 +0200

    works locally: 0 ... 0 5 10 18 33

--- participant_controller.m
+++ participant_controller.m
@@ -1,5 +1,9 @@
 % Basic participant controller.
 
+% list devices of robot.
+n_devices = wb_robot_get_number_of_devices();
+wb_console_print(int2str(n_devices), WB_STDOUT); % 13
+
 % Initialize the robot
 timestep = wb_robot_get_basic_time_step();
 
@@ -9,8 +13,8 @@ motor_right = wb_robot_get_device('wheel_right_joint');
 lidar = wb_robot_get_device('Hokuyo URG-04LX-UG01');
 position = wb_robot_get_device('gps');
 
-wb_motor_set_position(motor_left, float('inf'));
-wb_motor_set_position(motor_right, float('inf'));
+wb_motor_set_position(motor_left, inf);
+wb_motor_set_position(motor_right, inf);
 wb_motor_set_velocity(motor_left, 0);
 wb_motor_set_velocity(motor_right, 0);
 
