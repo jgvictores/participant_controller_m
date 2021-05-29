f5f5dd31ff496b2767ca69b4321cdae438ad8780
jgvictores <jcgvicto@gmail.com>
2021-05-29 16:29:19 +0200

    py -> m

--- participant_controller.m
+++ participant_controller.m
@@ -1,43 +1,20 @@
-#!/usr/bin/env python3
+% Basic participant controller.
 
-from controller import Robot, Lidar
+% Initialize the robot
+timestep = wb_robot_get_basic_time_step();
 
-# Initialize the robot
-robot = Robot()
-timestep = int(robot.getBasicTimeStep())
+% Initialize motors
+motor_left = wb_robot_get_device('wheel_left_joint');
+motor_right = wb_robot_get_device('wheel_right_joint');
+lidar = wb_robot_get_device('Hokuyo URG-04LX-UG01');
+position = wb_robot_get_device('gps');
 
-# Initialize motors
-motor_left = robot.getDevice('wheel_left_joint')
-motor_right = robot.getDevice('wheel_right_joint')
-lidar = robot.getDevice('Hokuyo URG-04LX-UG01')
-position = robot.getDevice('gps')
+wb_motor_set_position(motor_left, float('inf'));
+wb_motor_set_position(motor_right, float('inf'));
+wb_motor_set_velocity(motor_left, 0);
+wb_motor_set_velocity(motor_right, 0);
 
-poi_list = []
-poi_string_list = robot.getCustomData().split()
-
-for i in range(10):
-    poi_element = [float(poi_string_list[2*i]), float(poi_string_list[2*i+1])]
-    poi_list.append(poi_element)
-
-position.enable(timestep)
-lidar.enable(timestep)
-lidar_size = lidar.getHorizontalResolution()
-lidar_max_range = lidar.getMaxRange()
-
-motor_left.setPosition(float('inf'))
-motor_right.setPosition(float('inf'))
-motor_left.setVelocity(0)
-motor_right.setVelocity(0)
-
-while robot.step(timestep) != -1:
-    lidar_image = lidar.getRangeImage()
-
-    if lidar_image[lidar_size//2] > lidar_max_range/10:
-        motor_left.setVelocity(5)
-        motor_right.setVelocity(5)
-    elif poi_list[9][0] > position.getValues()[0]:
-        motor_left.setVelocity(0)
-        motor_right.setVelocity(0)
-    else:
-        motor_left.setVelocity(-2)
-        motor_right.setVelocity(2)
+while wb_robot_step(timestep) ~= -1
+  wb_motor_set_velocity(motor_left, 5);
+  wb_motor_set_velocity(motor_right, 5);
+end
