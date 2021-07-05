4ad9da49c1e38e60f5dd1ca7d19b1541fcf560a8
jgvictores <jcgvicto@gmail.com>
2021-07-05 19:47:31 +0200

    will want back

--- participant_controller.m
+++ participant_controller.m
@@ -1,24 +1,41 @@
-% Basic participant controller.
-
-% list devices of robot.
-n_devices = wb_robot_get_number_of_devices();
-wb_console_print(int2str(n_devices), WB_STDOUT); % 13
-
 % Initialize the robot
 timestep = wb_robot_get_basic_time_step();
 
-% Initialize motors
+% Obtain waypoints
+waypoints_string = wb_robot_get_custom_data();
+waypoints_flat = str2num(waypoints_string);
+waypoints_transposed = reshape(waypoints_flat,[2,10]);
+waypoints = transpose(waypoints_transposed);
+wb_console_print(sprintf('Waypoints: \n'), WB_STDOUT);
+for i = 1:10
+  wb_console_print(sprintf('[%f %f] ', waypoints(i,1), waypoints(i,2)), WB_STDOUT);
+end
+wb_console_print(sprintf('\n'), WB_STDOUT);
+
+% Initialize devices
 motor_left = wb_robot_get_device('wheel_left_joint');
 motor_right = wb_robot_get_device('wheel_right_joint');
-lidar = wb_robot_get_device('Hokuyo URG-04LX-UG01');
-position = wb_robot_get_device('gps');
+gps = wb_robot_get_device('gps');
+
+wb_motor_set_velocity(motor_left, 0.0);
+wb_motor_set_velocity(motor_right, 0.0);
 
 wb_motor_set_position(motor_left, inf);
 wb_motor_set_position(motor_right, inf);
-wb_motor_set_velocity(motor_left, 0);
-wb_motor_set_velocity(motor_right, 0);
 
+wb_gps_enable(gps, timestep);
+
+% Main loop:
+% - perform simulation steps until Webots is stopping the controller
 while wb_robot_step(timestep) ~= -1
-  wb_motor_set_velocity(motor_left, 5);
-  wb_motor_set_velocity(motor_right, 5);
+  pos = wb_gps_get_values(gps);
+  wb_console_print(sprintf('Hello World from MATLAB! [%f %f %f]\n', pos(1), pos(2), pos(3)), WB_STDOUT);
+  wb_motor_set_velocity(motor_left, 5.0);
+  wb_motor_set_velocity(motor_right, 5.0);
+  if pos(1) > -1.5
+    break
+  end
 end
+
+wb_console_print(sprintf('Bye from MATLAB!\n'), WB_STDOUT);
+quit;
